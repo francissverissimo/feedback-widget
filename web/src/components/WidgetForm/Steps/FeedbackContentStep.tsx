be7de495fcda75aba20bd430632e670553482613
Francis Verissimo <francissv97@gmail.com>
2022-06-16 17:06:28 -0400

    integrate web with server

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,9 +1,11 @@
 import { FormEvent, useState } from "react";
 import { ArrowLeft } from "phosphor-react";
 
+import { api } from "../../../lib/api";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
+import { Loading } from "../../Loading";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -14,18 +16,26 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
-  onFeedbackSent
+  onFeedbackSent,
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
 
-    console.log({ screenshot, comment });
+    setIsSendingFeedback(true);
 
+    await api.post(`/feedbacks`, {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -40,7 +50,11 @@ export function FeedbackContentStep({
           <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
         <span className="text-xl leading-6 flex items-center gap-2">
-          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
           {feedbackTypeInfo.title}
         </span>
 
@@ -51,7 +65,7 @@ export function FeedbackContentStep({
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontencendo"
-          onChange={event => setComment(event.target.value)}
+          onChange={(event) => setComment(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
@@ -63,7 +77,9 @@ export function FeedbackContentStep({
             type="submit"
             disabled={comment.length === 0}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
-          >Enviar feedback</button>
+          >
+            {isSendingFeedback ? <Loading /> : "Enviar feedback"}
+          </button>
         </footer>
       </form>
     </>
